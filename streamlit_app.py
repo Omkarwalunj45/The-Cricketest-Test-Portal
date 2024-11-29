0ec5b85874328f57b17d9f9514c469f8fe590da6
Omkar Walunj <168619829+Omkarwalunj45@users.noreply.github.com>
2024-11-29 13:07:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1789,7 +1789,7 @@ elif sidebar_option == "Match by Match Analysis":
             st.write(f"Batting Style: {batting_style}")
 
             # Set mirroring factor based on RHB or LHB
-            mirror_factor = -1 if batting_style == 'LHB' else 1
+            mirror_factor = 1 if batting_style == 'LHB' else -1
 
             # Plot points for each ball, excluding dot balls and rows with NaN in line or length
             for index, row in final_df.iterrows():
